aaf950e424f5539864502915bcd948793c32273f
Nick Farina <nfarina@gmail.com>
2011-02-21 18:12:50 -0800

    Added additional comments, plus MIT license.

--- SMWebRequest.h
+++ SMWebRequest.h
@@ -1,8 +1,39 @@
+/*
+ The MIT License
+ 
+ Copyright (c) 2010 Nick Farina
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE. 
+ */
 
 // SMWebRequest encapsulates a single HTTP request and response. It is designed to be less verbose
 // and simpler to use than NSURLConnection. The server response is buffered completely into memory
 // then passed back to event listeners as NSData. Optionally, you can specify a delegate which
 // can process the NSData in some way on a background thread then return something else.
+
+// While the SMWebRequest's internal connection to the server is open, it will retain itself.
+// This means you can create an autoreleased SMWebRequest without retaining it and subscribe
+// to its events for a quick and dirty (non-cancellable) request.
+
+// Please note that this means you MUST remove any event listeners before releasing
+// your SMWebRequest! Otherwise the request may still be alive and will attempt to execute
+// events on your object (which may be dealloc'd at this point).
 
 enum {
     SMWebRequestEventComplete  = 1 << 0, // selector will be passed the result pointer
