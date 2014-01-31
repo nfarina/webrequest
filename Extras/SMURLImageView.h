5721db5060af2ab13c6972a366e623ea6f08a014
Nick Farina <nfarina@gmail.com>
2014-01-31 07:55:05 -0800

    unsafe_unretained -> weak

--- Extras/SMURLImageView.h
+++ Extras/SMURLImageView.h
@@ -1,4 +1,3 @@
-
 //
 // UIImageView that loads its image asynchronously from a URL.
 //
@@ -7,7 +6,7 @@
 
 @interface SMURLImageView : UIImageView
 
-@property (nonatomic, unsafe_unretained) id<SMURLImageViewDelegate> delegate;
+@property (nonatomic, weak) id<SMURLImageViewDelegate> delegate;
 @property (nonatomic, copy) NSURL *imageURL;
 @property (nonatomic, assign) BOOL disableAutoSizeAdjustment; // default NO. set to YES if you do not want the bounds adjusted automatically to fit the loaded image.
 
